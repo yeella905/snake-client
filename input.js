1f0a1360306aef3dc979ba51be106923475ec48b
yeella905 <yeella905@gmail.com>
2024-12-20 13:27:54 -0500

    added proper wasd movements

--- input.js
+++ input.js
@@ -1,20 +1,31 @@
-
 let connection;
 
-
-const handleUserInput = function (key) {
-    if (key === "\u0003") {
-        process.exit();
-      }
-  };
 const setupInput = (conn) => {
     connection = conn;
     const stdin = process.stdin;  // create variable to hold the stdin object so we don't have to type process.stdin multiple times
+
     stdin.setRawMode(true); // Raw Mode allows us to listen for individual keypresses instead of waiting for the user to press enter
     stdin.setEncoding("utf8"); // utf8 encoding is set so that we can read the text data that is input
     stdin.resume(); // resume stdin so the program can listen for input
+
     stdin.on('data', handleUserInput);
     return stdin;   // return the stdin object so we can use it elsewhere in the program
   };
+
+const handleUserInput = (key) => {
+    if (key === "\u0003") {
+        process.exit();
+    }
+    if (key === 'w') {
+        connection.write('Move: up');
+      } else if (key === 'a') {
+        connection.write('Move: left');
+      } else if (key === 's') {
+        connection.write('Move: down');
+      } else if (key === 'd') {
+        connection.write('Move: right');
+      }
+  };
+
 
 module.exports = { setupInput };
