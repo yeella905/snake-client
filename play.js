0db6651c0f5f612613faabf51cec5987981c13cf
yeella905 <yeella905@gmail.com>
2024-12-20 11:15:21 -0500

    stdin set up

--- play.js
+++ play.js
@@ -1,3 +1,20 @@
 const { connect } = require('./client');
 
 connect();
+
+const setupInput = function () {
+    const stdin = process.stdin;  // create variable to hold the stdin object so we don't have to type process.stdin multiple times
+    stdin.setRawMode(true); // Raw Mode allows us to listen for individual keypresses instead of waiting for the user to press enter
+    stdin.setEncoding("utf8"); // utf8 encoding is set so that we can read the text data that is input
+    stdin.resume(); // resume stdin so the program can listen for input
+    return stdin;   // return the stdin object so we can use it elsewhere in the program
+  };
+
+  const handleUserInput = function (key) {
+    if (key === "\u0003") {
+        process.exit();
+      }
+  };
+
+  connect();
+  setupInput();
