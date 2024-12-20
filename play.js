606dd68806b39f193ac9a1c3f2a15e9a238874fa
yeella905 <yeella905@gmail.com>
2024-12-19 21:17:08 -0500

    included event handler

--- play.js
+++ play.js
@@ -3,15 +3,20 @@ const net = require("net");
 // establishes a connection with the game server
 const connect = function () {
   const conn = net.createConnection({
-    host: "10.255.255.254",// IP address here,
+    host: 'localhost',// IP address here,
     port: 50541// PORT number here,
   });
 
   // interpret incoming data as text
   conn.setEncoding("utf8");
 
+  conn.on('data', (data) => {
+    console.log('Server says: ', data);
+  });
+  
   return conn;
 };
 
 console.log("Connecting ...");
 connect();
+
