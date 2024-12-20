f3f07bc7a18b739d891bb004d0ebe27983bbec61
yeella905 <yeella905@gmail.com>
2024-12-20 14:07:18 -0500

    added constant values into a separate file

--- client.js
+++ client.js
@@ -1,9 +1,10 @@
 const net = require("net");
+const { IP, PORT } = require('./constants');
 
 const connect = function () {
   const conn = net.createConnection({
-    host: 'localhost', // IP address here
-    port: 50541 // PORT number here
+    host: IP, // IP address here
+    port: PORT // PORT number here
   });
 
   // interpret incoming data as text
---
+++ constants.js
@@ -0,0 +1,7 @@
+const IP = "10.255.255.254"; // replace with the actual IP address
+const PORT = 50541; // replace with the actual port number
+
+module.exports = {
+    IP,
+    PORT,
+  };
